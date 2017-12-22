3f80f5e71dcbd407238572b1dd39f32ebab43503
charlie <rec@Orchard.local>
2017-12-22 17:21:40 -0500

    Implemented more basic chrome storage functionality..

--- src/background/src/storage.js
+++ src/background/src/storage.js
@@ -11,44 +11,39 @@ export const init = () => {
     get(null)
       .then((results) => {
         if (results) {
-          console.log('Got chrome settings: ', results)
           if (!results.version || results.version !== defaults.version) {
             console.log('Settings need to be updated.')
+            set(defaults)
+            resolve(defaults)
           }
           resolve(results)
         } else {
           // load defaults
-          console.log('Loading default settings')
-          resolve('Need to load Defaults....')
+          console.log('Loading default settings....')
+          set(defaults)
+          resolve(defaults)
         }
       })
-    // return the current settings
   })
 }
 
-// get key
+// TODO: get and set are slightly backwards: as get should check for key type
+// but set dosnt need to?
+// get (by key) and return a setting, or all settings from storage
+// key can be a string or array of strings or null for all settings
 export const get = (key) => {
   return new Promise((resolve, reject) => {
     chrome.storage.sync.get(key, (results) => {
-      console.log(results)
-      if (Object.keys(results).length !== 0) {
-        resolve(JSON.parse(results))
-      } else {
-        resolve(null)
-      }
+      resolve(results)
     })
-    // get and return a setting, or all settings from storage
   })
 }
 
 // set key to value
-export const set = (key, value) => {
-  if (Array.isArray(key)) {
-    key.map((k) => {
-      const v = JSON.stringify(value)
-      chrome.storage.sync.set({key, v})
-    })
-  }
+// takes an object with key/value pairs
+export const set = (settings) => {
+  chrome.storage.sync.set(settings)
+  // TODO: error checking?
 }
 
 /*
@@ -72,10 +67,14 @@ settings:
 siteUrl: position, lotsize, offset, api-keys
 
 */
-const defaults  = {
-  version: '0.0.1',
+const settings = {
   offset: '0.1',
   lotsize: '0.001',
   multipliers: [0.1, 0.25, 0.5, 0.75, 1, 2, 3, 5, 10, 25, 50, 100],
   infoWindowPosition: {x: 0, y: 0}
+}
+
+const defaults = {
+  version: '0.0.2',
+  'gdax.com': settings
 }
